f56e444aa90d3b53a99b15b4ea5330e3c30d2d14
Michael Chilán <michael.chilan@epn.edu.ec>
2021-09-17 23:28:13 -0500

    Clase 08

--- Clases/01 NESTJS/web/src/app.controller.ts
+++ Clases/01 NESTJS/web/src/app.controller.ts
@@ -1,4 +1,4 @@
-import {BadRequestException, Controller, Get, HttpCode, InternalServerErrorException} from '@nestjs/common';
+import {BadRequestException, Controller, Get, HttpCode, InternalServerErrorException, Req, Res} from '@nestjs/common';
 import { AppService } from './app.service';
 
 @Controller()
@@ -24,5 +24,46 @@ export class AppController {
   holaJson(): string {
     return '{mensaje: "Hola json"}';
   }
+
+  // Clase 08
+  //tipo de errores
+  @Get('bad-request')
+  badRequest() {
+    throw new BadRequestException();
+  }
+
+  @Get('internal-error')
+  internalError() {
+    throw new InternalServerErrorException();
+  }
+
+  @Get('setear-cookie-insegura')
+  setearCookieInsegura(
+      @Req() req, // request - PETICION
+      @Res() res, // response - RESPUESTA
+  ) {
+    res.cookie(
+        'galletaInsegura', //nombre
+        'Tengo hambre', // valor
+    );
+    res.cookie(
+        'galletaSegura', //nombre
+        'Web :3', // valor
+        {
+          secure: true
+        }
+    );
+    res.send('ok'); // return de antes
+  }
+
+  @Get('mostrar-cookies')
+  mostrarCookies(@Req() req) {
+    const mensaje = {
+      sinFirmar: req.cookies,
+      firmadas: req.signedCookies,
+    };
+    return mensaje;
+  }
+
 
 }
